70313280a9a5681a45824ca9776aa24062208b42
Victor Carrasco <victor8577@gmail.com>
2013-07-18 18:19:47 +0200

    region, city and country are obligatory to add item

--- assets/js/item_add.js
+++ assets/js/item_add.js
@@ -5,5 +5,36 @@ $(document).ready(function(){
 	$('select#parentCategory').find('option:first').prop("selected",true);
 	$("select#parentCategory").trigger('change');
     }, 1) ;
-
+    $("form[name=item]").validate({
+        rules: {
+            country: {
+                required: true
+            },
+            region: {
+                required: true
+            },
+            city: {
+                required: true
+            }
+        },
+        messages: {
+            country : {
+                required: jobboard.langs.country_required
+            },
+            region : {
+                required: jobboard.langs.region_required
+            },
+            city : {
+                required: jobboard.langs.city_required
+            }
+        },
+        errorLabelContainer: "#flashmessage",
+        wrapper: "label",
+        invalidHandler: function(form, validator) {
+            $("#flashmessage").empty();
+            $("#flashmessage").addClass("flashmessage-error");
+            $("#flashmessage").append('<a class="btn ico btn-mini ico-close">x</a>');
+            $('html,body').animate({scrollTop: $('h1').offset().top}, {duration: 250, easing: 'swing'});
+        }
+    });
 });
