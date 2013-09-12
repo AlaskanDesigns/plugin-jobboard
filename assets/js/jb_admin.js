fe68339e5c634a45008f9654fb90ea28bb0cf8f3
Victor Carrasco <victor8577@gmail.com>
2013-09-12 17:45:42 +0200

    updated with minor changes

--- assets/js/jb_admin.js
+++ assets/js/jb_admin.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var admins = JSON.parse(jobboard.admin_users);
     $('#check_all').parent().after('<th>' + admins['admin_option'] + '</th>');
     $('.col-bulkactions [name="id[]"]').each(function(element, object) {
-        $(this).parent().after('<td class="user-type"><label>' + admins[$(this).val()] + '</label></td>');
+        $(this).parent().after('<td class="user-type">' + admins[$(this).val()] + '</td>');
     });
 
 });
