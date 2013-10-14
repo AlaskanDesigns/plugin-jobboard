2b1e0f7c564e665f6e87025b356cba0916d5c9db
Victor Carrasco <victor8577@gmail.com>
2013-10-14 11:55:43 +0200

    correct redirect to premium items

--- convert_item_premium.php
+++ convert_item_premium.php
@@ -1,13 +1,19 @@
 <?php
 $item_id = Params::getParam("item_id");
 
-$premium_item_url = "premium_ads/view/settings.php";
+$premium_item_url = "index.php";
 if($item_id) {
     $premium_item_url .= "?item_id=" . $item_id;
 }
 ?>
 
-<h1>CONVERT ITEM PREMIUM</h1>
+<h1>CONVERT ITEM TO PREMIUM</h1>
 <p><?php _e("parrafada para convertir el anuncio en premium", "premium_ads"); ?></p>
 
-<a class="btn btn-blue" href="<?php echo osc_admin_render_plugin_url($premium_item_url); ?>"><?php echo 'Convert item'; ?></a>
+<form id="form-convert-pa" action="<?php echo osc_admin_render_plugin_url("premium_ads/index.php"); ?>" name="form-comver-pa" method="post">
+    <input type="hidden" name="page" value="plugins" />
+    <input type="hidden" name="action" value="renderplugin" />
+    <input type="hidden" name="file" value="premium_ads/view/settings.php" />
+    <input type="hidden" name="item_id" value="<?php echo $item_id; ?>" />
+    <input type="submit" class="btn btn-blue" value="<?php echo 'Convert item'; ?>" />
+</form>
